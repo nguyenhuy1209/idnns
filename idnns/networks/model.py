bf1747c0c446bcac566d34538c8e755a643de892
Huy G. Nguyen <huy.g.nguyen@eastagile.com>
2024-03-25 19:29:09 +0700

    refactor to python

--- idnns/networks/model.py
+++ idnns/networks/model.py
@@ -1,30 +1,60 @@
+import torch
 import torch.nn as nn
 
+ACTIVATION_FUNCTION = {
+    0: nn.Tanh(),
+    1: nn.ReLU(),
+    2: nn.PReLU(),
+    3: nn.ELU(),
+    4: nn.Softplus(),
+    6: nn.SiLU(),
+    7: torch.abs,
+}
+
+
 class Model(nn.Module):
-	def __init__(self, activation_function, layer_sizes, input_size, num_of_classes, save_file, covn_net):
-		super(Model, self).__init__()
-		self.save_file = save_file
-		layer_sizes = [input_size] + layer_sizes
-		self.linears = nn.ModuleList([nn.Linear(layer_sizes[i], layer_sizes[i+1]) for i in range(len(layer_sizes)-1)])
-		self.last_layer = nn.Linear(layer_sizes[-1], num_of_classes)
-		self.activation = nn.Tanh() if activation_function == 0 else nn.ReLU()
-		self.softmax = nn.Softmax(dim=1) # for MI calculation only, not for loss calculation
-        
-	def forward(self, x):
-		# x: shape (B, D)
-		layers = []
-		for layer in self.linears:
-			x = layer(x)
-			x = self.activation(x)
-			layers.append(x)
+    def __init__(
+        self,
+        activation_function,
+        layer_sizes,
+        input_size,
+        num_of_classes,
+        save_file,
+        covn_net,
+    ):
+        super(Model, self).__init__()
+        self.save_file = save_file
+        layer_sizes = [input_size] + layer_sizes
+        self.linears = nn.ModuleList(
+            [
+                nn.Linear(layer_sizes[i], layer_sizes[i + 1])
+                for i in range(len(layer_sizes) - 1)
+            ]
+        )
+        self.last_layer = nn.Linear(layer_sizes[-1], num_of_classes)
+        # self.activation = nn.Tanh() if activation_function == 0 else nn.ReLU()
+        self.activation = ACTIVATION_FUNCTION[activation_function]
+        self.softmax = nn.Softmax(
+            dim=1
+        )  # for MI calculation only, not for loss calculation
 
-		x = self.last_layer(x)
-		layers.append(self.softmax(x))
+    def forward(self, x):
+        # x: shape (B, D)
+        layers = []
+        for layer in self.linears:
+            x = layer(x)
+            x = self.activation(x)
+            layers.append(x)
 
-		return x, layers
-	
-if __name__ == '__main__':
-	import torch
-	input = torch.randn(1, 12, requires_grad=True)
-	model = Model(0, [10, 7, 5, 4, 3], 12, 2, None, None)
-	pred, layers = model(input)
+        x = self.last_layer(x)
+        layers.append(self.softmax(x))
+
+        return x, layers
+
+
+if __name__ == "__main__":
+    import torch
+
+    input = torch.randn(1, 12, requires_grad=True)
+    model = Model(0, [10, 7, 5, 4, 3], 12, 2, None, None)
+    pred, layers = model(input)
--- main.py
+++ main.py
@@ -1,35 +1,37 @@
 """
 Train % plot networks in the information plane
 """
+
 from idnns.networks import information_network as inet
+
 
 def main():
     # Build the network
-    print('Building the network')
+    print("Building the network")
     args = {
-        "d_name": "var_u",
+        "d_name": "MNIST",
         "save_ws": True,
         "calc_information": True,
         "calc_information_last": True,
         "save_grads": True,
         "num_epochs": 10000,
-        "num_of_repeats": 10,
+        "num_of_repeats": 1,
         "num_of_samples": 10000,
-        "activation_function": 0,
-        "batch_size": 4096,
+        "activation_function": 7,
+        "batch_size": 300,
         "learning_rate": 0.004,
-        "net_type": "[10,8,6,4]", # Fig 3
+        "net_type": "[512,256,128,64]",  # Fig 3
     }
     net = inet.informationNetwork(args_additional=args)
     net.print_information()
-    print('Start running the network')
+    print("Start running the network")
     net.run_network()
-    print('Saving data')
+    print("Saving data")
     net.save_data()
-    print('Ploting figures')
-    net.dir_saved = 'jobs/net_sampleLen=1_nDistSmpls=1_layerSizes=10,8,6,4_nEpoch=10000_batch=4096_nRepeats=10_nEpochInds=3496_LastEpochsInds=9998_DataName=var_u_lr=0.004_tanh'
+    print("Ploting figures")
+    # net.dir_saved = "/Users/huy/Code/idnns/jobs/net_sampleLen=1_nDistSmpls=1_layerSizes=10,8,6,4_nEpoch=10000_batch=4096_nRepeats=5_nEpochInds=3496_LastEpochsInds=9998_DataName=var_u_lr=0.004_renyi_abs"
     net.plot_network()
 
-if __name__ == '__main__':
-    main()
 
+if __name__ == "__main__":
+    main()
